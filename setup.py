59ec7f3445dd8950a92be79c77ce89351f201b3d
Manatsawin Hanmongkolchai <git@whs.in.th>
2022-12-21 21:05:03 +0700

    Bump cffi

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="libthai-cffi",
-    version="1.0.1",
+    version="1.0.2",
     url="https://github.com/whs/libthai-cffi",
     author="Manatsawin Hanmongkolchai",
-    author_email="manatsawin+pypi@gmail.com",
+    author_email="pypi@whs.in.th",
     description="CFFI Binding to libthai",
     license="LGPL",
     packages=find_packages(exclude=["test"]),
-    setup_requires=["cffi~=1.11.5"],
-    install_requires=["cffi~=1.11.5"],
+    setup_requires=["cffi~=1.15.1"],
+    install_requires=["cffi~=1.15.1"],
     cffi_modules=["package/build_cffi.py:ffibuilder"],
 )
